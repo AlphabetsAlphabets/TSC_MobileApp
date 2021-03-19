00f5fca4f7d89410f6dbe96760654545d0da3e91
YJH16120 <yapjiahong2003@gmail.com>
2021-03-19 14:00:21 +0800

    Previously messed up config of a few things. Decided to migrate and restart.

--- README.md
+++
@@ -1,9 +0,0 @@
-# Tong San Chan (TSC) Mobile App
-What the mobile App is built on:
-- Xamarin
-- C# with .NET Framework (.NET 5.0)
-
-What it can do:
-- Scan QR code,
-- Upload an image (or images) to the server remotely via a RESTful API written in Python,
-- check whether or not an employee is within the bounds of an area. Specifically the bounds of the dealer's compound.
---
+++ TSC_Mobile/TSC_Mobile/LoginPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Plugin.Toast;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TSC_Mobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+        public Page mainPage = new MainPage();
+
+        public static string uri = MainPage.uri;
+
+        private async void ValidateCredentials(object sender, EventArgs e)
+        {
+            var password = PasswordField.Text;
+            var username = UsernameField.Text;
+            var resource = $"{username}/{password}";
+
+            var credentials = await Request.Login(uri, resource);
+            if (credentials.Message != null) {
+                alert.Text = "Incorrect credentials.";
+                return;
+            }
+
+            CrossToastPopUp.Current.ShowToastMessage("Logged in successfully.");
+            await Navigation.PushModalAsync(mainPage);
+        }
+    }
+}
